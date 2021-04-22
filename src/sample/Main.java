8a8d28f6c5df8a4750d6d259595d0c367ea4fa76
LauraWolf <wolflaura03@gmail.com>
2021-04-22 11:29:57 -0300

    Quinto Commit

---
+++ src/sample/Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+import sample.model.Pessoa;
+
+
+public class Main  {
+
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("Laura");
+        pessoa.setContato("90-999-2004","OwOUwU@email","@aaaaaa");
+        System.out.println(pessoa);
+
+    }
+}
---
+++ src/sample/model/Contato.java
@@ -0,0 +1,40 @@
+package sample.model;
+
+public class Contato {
+    private String whatsapp;
+    private String email;
+    private String twitter;
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "whatsapp='" + whatsapp + '\'' +
+                ", email='" + email + '\'' +
+                ", twitter='" + twitter + '\'' +
+                '}';
+    }
+}
